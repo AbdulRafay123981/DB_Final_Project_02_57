ebc58907e24f2af2a0462caa7817ff4a74bb78e0
Abdul Rafay <iamar544473@gmail.com>
2026-05-24 22:40:25 +0500

    Added Client Logic queries

---
+++ OffsetPrintingSystem/OffsetPrintingSystem/Logic/ClientLogic.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using OffsetPrintingSystem.Business;
+
+namespace OffsetPrintingSystem.Logic
+{
+    public class ClientLogic
+    {
+        public List<Client> GetAllClients()
+        {
+            List<Client> list = new List<Client>();
+            string query = "SELECT * FROM clients";
+            MySqlDataReader reader = DatabaseHelper.Instance.getData(query);
+            while (reader.Read())
+            {
+                Client c = new Client();
+                c.SetId(Convert.ToInt32(reader["id"]));
+                c.SetName(reader["name"].ToString());
+                c.SetContactPerson(reader["contact_person"].ToString());
+                c.SetPhone(reader["phone"].ToString());
+                c.SetEmail(reader["email"].ToString());
+                c.SetAddress(reader["address"].ToString());
+                list.Add(c);
+            }
+            reader.Close();
+            return list;
+        }
+
+        public void AddClient(Client c)
+        {
+            string query = $"INSERT INTO clients (name, contact_person, phone, email, address) " +
+                           $"VALUES ('{c.GetName()}', '{c.GetContactPerson()}', '{c.GetPhone()}', '{c.GetEmail()}', '{c.GetAddress()}')";
+            DatabaseHelper.Instance.Update(query);
+        }
+
+        public void UpdateClient(Client c)
+        {
+            string query = $"UPDATE clients SET name = '{c.GetName()}', contact_person = '{c.GetContactPerson()}', " +
+                           $"phone = '{c.GetPhone()}', email = '{c.GetEmail()}', address = '{c.GetAddress()}' WHERE id = {c.GetId()}";
+            DatabaseHelper.Instance.Update(query);
+        }
+
+        public void DeleteClient(int id)
+        {
+            string query = $"DELETE FROM clients WHERE id = {id}";
+            DatabaseHelper.Instance.Update(query);
+        }
+    }
+}
---
+++ OffsetPrintingSystem/OffsetPrintingSystem/Logic/DatabaseHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace OffsetPrintingSystem.Logic
+{
+    public class DatabaseHelper
+    {
+        private string serverName = "127.0.0.1";
+        private string port = "3306";
+        private string databaseName = "offsetprintingdb";
+        private string databaseUser = "root";
+        private string databasePassword = "601598#123";
+
+        private DatabaseHelper() { }
+
+        private static DatabaseHelper _instance;
+        public static DatabaseHelper Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new DatabaseHelper();
+                return _instance;
+            }
+        }
+
+        public MySqlConnection getConnection()
+        {
+            string cs = $"server={serverName};port={port};user={databaseUser};database={databaseName};password={databasePassword};SslMode=Required;";
+            MySqlConnection conn = new MySqlConnection(cs);
+            conn.Open();
+            return conn;
+        }
+
+        public MySqlDataReader getData(string query)
+        {
+            MySqlConnection conn = getConnection();
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            return cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+        }
+
+        public int Update(string query)
+        {
+            using (MySqlConnection conn = getConnection())
+            using (MySqlCommand cmd = new MySqlCommand(query, conn))
+            {
+                return cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
